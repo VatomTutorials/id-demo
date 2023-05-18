5b3b08faec707cf260f4053909950d18d6c5f65a
Joe Sandmeyer <github.com@unconscious.com>
2023-05-18 19:16:12 -0400

    Test ID

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useEffect, useRef } from 'react'
+import {VatomIdentitySDK} from "@vatom/identity-sdk"
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const divRef = useRef(null);
+
+  const authority = "https://id.vatom.com"
+  const clientId = "Xc4PN_Fri5WbrYokjbwg3" // Just an ID; do not expose secret in repo.
+
+  const onLogin= (data) =>{
+    console.log(data)
+  }
+
+  useEffect(() => {
+    if(divRef.current){
+      new VatomIdentitySDK(divRef.current, authority, clientId,onLogin)
+    }
+  }, [divRef])
 
   return (
-    <>
-      <div>
-        <h1>Here we go.</h1>
-      </div>
-    </>
+    <div className="App" ref={divRef}>
+
+    </div>
   )
 }
 
 export default App
+
+
+
+//         <h1>Here we go. &nbsp; Here we go. &nbsp; Here we go.</h1>
