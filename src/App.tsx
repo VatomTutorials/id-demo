7c8242258aa028a7af160a56faca1d9fc8a8047f
Joe Sandmeyer <github.com@unconscious.com>
2023-05-22 11:38:48 -0400

    Test with whitelisted client IDs

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,14 @@ function App() {
   const divRef = useRef(null);
 
   const authority = "https://id.vatom.com"
-  const clientId = "Xc4PN_Fri5WbrYokjbwg3" // Just an ID; do not expose secret in repo.
+  
+  // Test with one of the following clientIds.
+  // They are just an IDs; do not expose secrets in repo.
+  // They are whitelisted for testing on localhost ports 3000, 8000, & 8080.
+  // Dev server ports can be configured in '/id-demo/vite.config.ts'.
+  
+  //const clientId = "c8a80a8b11235f15" // Will Pulier
+  const clientId = "dce3993b6a24c278" // Vatom Developer Playground
 
   const onLogin= (data) =>{
     console.log(data)
